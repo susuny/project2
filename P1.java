777e3c6f6bccb984241a4ad44783c0b666be45fd
susuny <82340941+susuny@users.noreply.github.com>
2021-04-21 18:29:14 +0300

    P1
    Simple Processing file for eclipse

---
+++ P1.java
@@ -0,0 +1,33 @@
+
+import processing.core.PApplet;
+
+public class P1 extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+		    // The argument passed to main must match the class name
+		
+		        PApplet.main("P1");
+		    }
+
+		    // method used only for setting the size of the window
+		    public void settings(){
+		        
+		    }
+
+		    // identical use to setup in Processing IDE except for size()
+		    public void setup(){
+		        
+		    }
+
+		    // identical use to draw in Processing IDE
+		    public void draw(){
+
+		    }
+		
+	}
+
+
